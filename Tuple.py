dcc3318729f600a63f9c95a7a4800dc9fd2cea44
Swaichha <swaichhas@gamil.com>
2025-07-09 19:22:08 +0530

    learn py

--- Tuple.py
+++ Tuple.py
@@ -1,3 +1,16 @@
 
 tuple1=(1,2,3,4,5)
 print(tuple1)
+print(len(tuple1))
+print(tuple1.count(3))
+res=tuple1.index(3,0,4)
+print(res)
+print(type(tuple1))
+li=list(tuple1)
+print(type(li))
+letter="my name is {}"
+name="swaichha"
+print(letter.format(name))
+
+print(f"hey my name is{ {name}}")
+print(f"dKSHDEEP {name}");
